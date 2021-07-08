5e131b3127664f5fb87c2686ecd5d21440da7340
gshong <hgs33hgs@gmail.com>
2021-07-08 22:32:19 +0900

    기본 틀 구성

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
-function a() {
+import systeminfo from 'systeminformation';
 
-}
+
+setInterval(() => {
+    systeminfo.mem((data) => {
+        console.log(data.free / (1024 * 1024) + ' MB');
+    });
+    systeminfo.cpuCurrentSpeed((data) => {
+        console.log(data);
+    });
+}, 3000);
