62ed4dc0d25df9a5ff92e6fd6643943cd01f9a2f
gshong <hgs33hgs@gmail.com>
2021-07-08 23:10:27 +0900

    CPU 사용률 출력

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
 import systeminfo from 'systeminformation';
 
+import {cpuManager} from './cpu_manager';
 
-setInterval(() => {
-    systeminfo.mem((data) => {
-        console.log(data.free / (1024 * 1024) + ' MB');
-    });
-    systeminfo.cpuCurrentSpeed((data) => {
-        console.log(data);
-    });
-}, 3000);
+setInterval(async ()=>{
+    console.log(await cpuManager.getCpuPerTotal(4000));
+}, 5000);
+
+
+
+// setInterval(() => {
+//     systeminfo.mem((data) => {
+//         console.log(data.free / (1024 * 1024) + ' MB');
+//     });
+//     systeminfo.cpuCurrentSpeed((data) => {
+//         console.log(data);
+//     });
+// }, 3000);
