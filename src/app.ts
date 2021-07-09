e71fca07d4436bb5cbf967f9c30c0d2e0e14187b
gshong <gshong@innodep.com>
2021-07-09 14:13:32 +0900

    기본 틀 구현

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,32 @@
+import { cpuManager } from './cpu_manager';
+import { resourceManager } from './resource_manager';
+import { simpleUtils } from './simple_utils';
 import systeminfo from 'systeminformation';
 
-import {cpuManager} from './cpu_manager';
+import os from 'os';
 
-setInterval(async ()=>{
-    console.log(await cpuManager.getCpuPerTotal(4000));
-}, 5000);
+//console.log(os.cpus());
+//cpuManager.startPrintCpuUsage(5000);
 
+async function main() {
+    // setInterval(async () => {
+    //     console.log(await resourceManager.getMemoryInfo());
+    //     console.log(await resourceManager.getMemoryPerUsed());
+    // }, 4000);
+    // console.log('a1');
+    // console.log(await resourceManager.getDiskIoInfo());
+    // systeminfo.diskLayout(data => {
+    //     console.log('a1');
+    //     console.log(data);
+    // });
 
+    //console.log(await resourceManager.getFsInfo());
+    setInterval(async () => {
+        console.log(await resourceManager.getNetworkInfo());
+    }, 5000);
+}
+main();
 
-// setInterval(() => {
-//     systeminfo.mem((data) => {
-//         console.log(data.free / (1024 * 1024) + ' MB');
-//     });
-//     systeminfo.cpuCurrentSpeed((data) => {
-//         console.log(data);
-//     });
-// }, 3000);
+// setInterval(async () => {
+//     console.log(await cpuManager.getCpuPerTotal(5000));
+// }, 5000);
--- src/cpu_manager.ts
+++ src/cpu_manager.ts
@@ -1,15 +1,17 @@
 import os from 'os';
 
+type CpuTarget = 'user' | 'idle' | 'nice' | 'irq' | 'sys' | 'total';
+
 class cpu_manager {
     constructor() {}
 
-    private getCpuFreeTotalTimeMs() {
+    private getCpuTotalTimeMs(targetName: CpuTarget) {
         let total = 0;
-        let free = 0;
+        let target = 0;
 
         let cpuInfoArr = os.cpus();
         cpuInfoArr.map(cpuInfo => {
-            free += cpuInfo.times.idle;
+            target += cpuInfo.times[targetName];
         });
 
         cpuInfoArr.map(cpuInfo => {
@@ -20,37 +22,40 @@ class cpu_manager {
 
         return {
             total: total,
-            free: free,
+            target: target,
         };
     }
 
-    public async getCpuPerTotal(intervalMs: number): Promise<number> {
+    public async getCpuPerCommon(targetName: CpuTarget, intervalMs: number): Promise<number> {
+        if (targetName === 'total') {
+            let idleUsage = await this.getCpuPerCommon('idle', intervalMs);
+            return 100 - idleUsage;
+        }
+
         return new Promise(async resolve => {
-            let freeBefore = 0,
+            let targetBefore = 0,
                 totalBefore = 0;
-            let cpuInfo = this.getCpuFreeTotalTimeMs();
-            freeBefore = cpuInfo.free;
+            let cpuInfo = this.getCpuTotalTimeMs(targetName);
+            targetBefore = cpuInfo.target;
             totalBefore = cpuInfo.total;
             setTimeout(async () => {
-                let free = 0,
+                let target = 0,
                     total = 0;
-                cpuInfo = this.getCpuFreeTotalTimeMs();
-                free = cpuInfo.free - freeBefore;
+                cpuInfo = this.getCpuTotalTimeMs(targetName);
+                target = cpuInfo.target - targetBefore;
                 total = cpuInfo.total - totalBefore;
-                let usage = 100 - (free * 100) / total;
+                let usage = (target * 100) / total;
                 resolve(usage);
             }, intervalMs);
         });
     }
 
-    public async getCpuPerFree(intervalMs: number): Promise<number> {
-        let cpuPerTotal = await this.getCpuPerTotal(intervalMs);
-        return 100 - cpuPerTotal;
-    }
-
     public async startPrintCpuUsage(collectIntervalMs: number) {
-        console.log(`cpu per total : ${Math.round(await this.getCpuPerTotal(collectIntervalMs))} %`);
-        console.log(`cpu per free : ${Math.round(await this.getCpuPerFree(collectIntervalMs))} %`);
+        console.log(`cpu per total : ${(await this.getCpuPerCommon('total', collectIntervalMs)).toFixed(3)} %`);
+        console.log(`cpu per free : ${(await this.getCpuPerCommon('idle', collectIntervalMs)).toFixed(3)} %`);
+        console.log(`cpu per user : ${(await this.getCpuPerCommon('user', collectIntervalMs)).toFixed(3)} %`);
+        console.log(`cpu per irq : ${(await this.getCpuPerCommon('irq', collectIntervalMs)).toFixed(3)} %`);
+        console.log(`cpu per sys : ${(await this.getCpuPerCommon('sys', collectIntervalMs)).toFixed(3)} %`);
         this.startPrintCpuUsage(collectIntervalMs);
     }
 }
